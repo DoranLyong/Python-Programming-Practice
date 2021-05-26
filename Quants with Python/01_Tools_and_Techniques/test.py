b2619245b33628d0527b44f2ff3afd99ed4c1253
DoranLyong <cheeryun@gmail.com>
2021-05-26 15:43:16 +0900

    Quants

--- Quants with Python/01_Tools_and_Techniques/test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
-from scipy.interpolate import interp2d
-from scipy.linalg import inv, solve, det, eig
---
+++ Quants with Python/README.md
@@ -0,0 +1,6 @@
+# Python Practice for Quants 
+
+
+*** 
+### Reference 
+[1] [Quantitative Economics with Python](https://python.quantecon.org/intro.html) / 이거 보고 따라하기 <br/>
